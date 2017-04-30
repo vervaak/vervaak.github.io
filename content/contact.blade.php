62f3732bc7c76c48e6ea9125f14be6dd2dbf11a0
vervaak <sijung.lai@gmail.com>
2017-05-01 00:36:39 +0900

    fix css 3

--- content/contact.blade.php
+++ content/contact.blade.php
@@ -15,15 +15,12 @@
         min-width: 100% !important;
     }
 }
-	<style>
-	<div>
+	</style>
 		<img class="splash-images" src="@url('assets/images/contact_me.jpg')" />
 		<br/>
 		<h3>歡迎您於我聯絡！</h3>
 
 		email：sculptorlai@gmail.com<br/>
 		tel：+886 0928-625-372
-	</div>
-
 
 @stop
