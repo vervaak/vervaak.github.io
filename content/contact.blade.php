dfb1316cddc99a0581026ea874314e3e1800c78b
vervaak <sijung.lai@gmail.com>
2017-05-01 00:39:29 +0900

    fix css 5

--- content/contact.blade.php
+++ content/contact.blade.php
@@ -3,20 +3,8 @@
 @section('pageTitle', '- Contact')
 
 @section('body')
-	<style>
-#splash-images{
-    width: 70% !important;
-    min-width: 720px !important;
-}
 
-@media only screen and (max-width: 550px) {
-    #splash-images{
-        width: 100% !important;
-        min-width: 100% !important;
-    }
-}
-	</style>
-		<img id="splash-images" src="@url('assets/images/contact_me.jpg')" />
+		<img class="splash-images" src="@url('assets/images/contact_me.jpg')" />
 		<br/>
 		<h3>歡迎您於我聯絡！</h3>
 
